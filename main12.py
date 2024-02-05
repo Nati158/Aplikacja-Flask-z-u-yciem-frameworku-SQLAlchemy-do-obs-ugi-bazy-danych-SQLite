38e4a377f00f72530c32544876de1356227f9e9c
Nati158 <141584277+Nati158@users.noreply.github.com>
2024-02-05 21:53:19 +0100

    Add files via upload

---
+++ main12.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+@app.route('/')
+def index():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get(id)
+    return render_template('post.html', post=post)
+
+@app.route('/new_post', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('new_post.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
